10f4dd2e76856721246320cfe2eb7f8d9bfa550e
Pakillo <f.rodriguez.sanc@gmail.com>
2019-01-20 23:08:16 +0100

    add function to simulate data

---
+++ R/simul_data.R
@@ -0,0 +1,60 @@
+
+
+#' Simulate dataset
+#'
+#' @param nspp Number of taxa
+#' @param nsite Number of sites
+#' @param seed Random seed
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @seealso http://www.cookbook-r.com/Numbers/Saving_the_state_of_the_random_number_generator/
+
+
+simul_data <- function(nspp, nsite, seed) {
+
+  # simulate a phylogenetic tree
+  phy <- ape::rtree(n = nspp)
+  phy <- ape::compute.brlen(phy, method = "Grafen", power = 0.5)
+
+  # standardize the phylogenetic covariance matrix to have determinant 1
+  Vphy <- ape::vcv(phy)
+  Vphy.std <- Vphy/(det(Vphy)^(1/nspp))
+
+  # Perform a Cholesky decomposition of Vphy
+  iD <- t(chol(Vphy.std))
+
+  # Generate environmental site variable
+  env <- seq(0, 20, length.out = nsite)
+
+
+
+  ## Set up species-specific regression coefficients as random effects
+  intercept <- iD %*% runif(nspp, -1, 2)
+  slope <- iD %*% runif(nspp, -0.4, 0.1)
+
+  intercept <- intercept[gtools::mixedorder(rownames(intercept)), ]
+  slope <- slope[gtools::mixedorder(rownames(slope)), ]
+
+
+
+  ## Calculate suitabilities for each taxa and site
+  suitab <- rep(intercept, each = nsite)
+  suitab <- suitab + rep(slope, each = nsite) * rep(env, nspp)
+  suitab.error <- suitab + rnorm(nspp * nsite, mean = 0, sd = 1) #add some random 'error'
+  suitab.invlogit <- arm::invlogit(suitab)
+  suitab.error.invlogit <- arm::invlogit(suitab.error)
+
+
+  # Generate presence-absence data from suitabilities
+  pres <- rbinom(length(suitab.error), size = 1, prob = suitab.error.invlogit)
+
+
+  dat <- data.frame(taxa = paste("t", sort(rep(1:nspp, nsite)), sep = ""),
+                    site = rep(1:nsite, nspp),
+                    env = rep(env, nspp),
+                    suitab.invlogit = suitab.invlogit,
+                    presabs = pres)
+
+}
