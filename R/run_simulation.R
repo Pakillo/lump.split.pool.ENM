8b867cdbaf73d6c43fc8fc4c563cf688267d7e19
Pakillo <f.rodriguez.sanc@gmail.com>
2019-01-23 03:43:02 +0100

    Add fun to run replicate simulations

--- R/run_simulation.R
+++ R/run_simulation.R
@@ -6,7 +6,7 @@
 #' @return A data.frame
 #' @export
 #'
-run_simulation <- function(nspp, nsite, delta = 0.95) {
+run_simulation <- function(nspp, nsite, delta) {
 
   simdata <- simul_data(nspp, nsite)
 
@@ -25,3 +25,32 @@ run_simulation <- function(nspp, nsite, delta = 0.95) {
   results
 
 }
+
+
+
+#' Run a number of simulations with fixed parameters
+#'
+#' @param nsim Number of replicate simulations to run
+#' @inheritParams run_simulation
+#'
+#' @return A data.frame with nrow = nspp*nsim
+#' @export
+#'
+
+run_sims <- function(nsim, nspp, nsite, delta) {
+
+  file.remove("pglmm.rds")  # delete file to force model compiling for first run
+
+  reps.list <- replicate(nsim, run_simulation(nspp, nsite, delta), simplify = FALSE)
+
+  reps.df <- do.call("rbind", reps.list)
+
+}
+
+
+# gives error with brms:
+# run_sims_params <- function(nsim, nspp, nsite, delta) {
+#
+#   sims <- purrr::map2_dfr(as.list(nspp), as.list(nsite), run_sims_fixed, nsim, delta)
+#
+# }
